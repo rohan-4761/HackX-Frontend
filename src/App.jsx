65ea21aae214fe27296b3ba23c261ef5c385b530
Dhruv <dhruvsuvarna2019@gmail.com>
2023-09-15 15:03:39 +0530

    latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Appbar from "./components/Appbar";
 import Login from "./components/login";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <BrowserRouter>
+      <Appbar />
       <Routes>
         <Route path="/login" element={<Login/>} />
       </Routes>
