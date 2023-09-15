0fff74599af9dc2f981e21d1dabdbe6903e766de
Dhruv <dhruvsuvarna2019@gmail.com>
2023-09-15 14:36:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Appbar from "./components/Appbar";
 import Login from "./components/login";
